380204a344960f884c07a544210c19167ddcbf4c
OlgaZheneva <olgazhenevasokol@gmail.com>
2025-01-04 17:04:01 +0300

    стиль кавычек

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,12 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 const config = [
-  // Общие настройки для всех файлов
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -17,6 +16,11 @@ const config = [
         sourceType: 'module',
       },
     },
+    rules: {
+      indent: ['error', 2],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+    },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
@@ -24,13 +28,8 @@ const config = [
   {
     settings: {
       react: {
-        version: 'detect', // Автоматическое определение версии React
+        version: 'detect',
       },
-    },
-    rules: {
-      indent: ['error', 2],
-      quotes: ['error', 'single'],
-      semi: ['error', 'always'],
     },
   },
 ];
